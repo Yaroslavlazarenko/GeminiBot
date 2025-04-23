160e3e979ac6ce20a634ce635d0369e2c28a7371
yaroslavlazarenko <yaroslav.lazarenko.odessa@gmail.com>
2025-04-23 17:15:46 +0300

    update migrations

--- alembic/versions/e6de35247b4f_add_video_note_and_transcribe_fields.py
+++ alembic/versions/e6de35247b4f_add_video_note_and_transcribe_fields.py
@@ -1,6 +1,6 @@
-"""add responds_to_video_note field to users
+"""add video note and transcribe fields
 
-Revision ID: f714ffc19093
+Revision ID: e6de35247b4f
 Revises: 2419e308fb27
 Create Date: 2024-04-23 14:00:00.000000
 
@@ -12,13 +12,16 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'f714ffc19093'
+revision: str = 'e6de35247b4f'
 down_revision: Union[str, None] = '2419e308fb27'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
+    # Add transcribe_voice_only column to users table
+    op.add_column('users', sa.Column('transcribe_voice_only', sa.Boolean(), server_default='false', nullable=False))
+    
     # Add responds_to_video_note column to users table
     op.add_column('users', sa.Column('responds_to_video_note', sa.Boolean(), server_default='true', nullable=False))
 
@@ -26,3 +29,6 @@ def upgrade() -> None:
 def downgrade() -> None:
     # Remove responds_to_video_note column from users table
     op.drop_column('users', 'responds_to_video_note')
+    
+    # Remove transcribe_voice_only column from users table
+    op.drop_column('users', 'transcribe_voice_only')
