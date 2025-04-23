11a0339f3432f232b947c78fbdb3e2b94a0cfb1f
yaroslavlazarenko <yaroslav.lazarenko.odessa@gmail.com>
2025-04-23 17:45:52 +0300

    edit commands

--- telegram/handlers/commands/__init__.py
+++ telegram/handlers/commands/__init__.py
@@ -1,7 +1,9 @@
 from .clear import router as clear_router
 from .inline_menu import router as inline_menu_router
+from .help import router as help_router
 
 command_routers = [
     clear_router,
     inline_menu_router,
+    help_router,
 ]
