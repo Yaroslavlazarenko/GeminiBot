eaa0c751a280e15a69294e6fd7f85a00b492c637
yaroslavlazarenko <yaroslav.lazarenko.odessa@gmail.com>
2025-04-23 17:11:37 +0300

    edit migrations

--- alembic/versions/f714ffc19093_add_responds_to_video_note.py
+++ alembic/versions/f714ffc19093_add_responds_to_video_note.py
@@ -1,7 +1,7 @@
 """add responds_to_video_note field to users
 
 Revision ID: f714ffc19093
-Revises: merge_heads_20240423
+Revises: 2419e308fb27
 Create Date: 2024-04-23 14:00:00.000000
 
 """
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'f714ffc19093'
-down_revision: Union[str, None] = 'merge_heads_20240423'
+down_revision: Union[str, None] = '2419e308fb27'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
