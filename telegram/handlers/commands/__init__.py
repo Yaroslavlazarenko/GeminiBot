b939e760937bd541aa4d0fa9fa59db5b0d162e83
yaroslavlazarenko <yaroslav.lazarenko.odessa@gmail.com>
2025-04-29 15:59:27 +0300

    edit init, add groupinlinemenu

--- telegram/handlers/commands/__init__.py
+++ telegram/handlers/commands/__init__.py
@@ -1,9 +1,11 @@
 from .clear import router as clear_router
 from .inline_menu import router as inline_menu_router
+from .group_inline_menu import router as group_inline_menu_router
 from .help import router as help_router
 
 command_routers = [
     clear_router,
     inline_menu_router,
+    group_inline_menu_router,
     help_router,
 ]
