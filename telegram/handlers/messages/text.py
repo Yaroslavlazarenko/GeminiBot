d1d02b78766d6eb909ec5b9cf543ac659f4012e0
yaroslavlazarenko <yaroslav.lazarenko.odessa@gmail.com>
2025-05-04 18:04:47 +0300

    edit bug

--- telegram/handlers/messages/text.py
+++ telegram/handlers/messages/text.py
@@ -1,7 +1,8 @@
 import logging
 
 from aiogram import F, Router
-from aiogram.types import Message, ChatType
+from aiogram.types import Message
+from aiogram.enums import ChatType
 from aiogram.exceptions import TelegramBadRequest, TelegramNetworkError, TelegramForbiddenError
 
 from ai.gemini_client import get_text_response
