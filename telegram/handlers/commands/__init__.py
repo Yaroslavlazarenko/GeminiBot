f916a752523c8b5ca439d38685ea575a4321a510
yaroslavlazarenko <yaroslav.lazarenko.odessa@gmail.com>
2025-05-25 18:24:26 +0300

    edit init

--- telegram/handlers/commands/__init__.py
+++ telegram/handlers/commands/__init__.py
@@ -1,11 +1,9 @@
-from .clear import router as clear_router
-from .inline_menu import router as inline_menu_router
-from .group_inline_menu import router as group_inline_menu_router
+from aiogram import Router
+from .start import router as start_router
 from .help import router as help_router
+from .settings import router as settings_router
 
-command_routers = [
-    clear_router,
-    inline_menu_router,
-    group_inline_menu_router,
-    help_router,
-]
+router = Router()
+router.include_router(start_router)
+router.include_router(help_router)
+router.include_router(settings_router)
--- telegram/handlers/messages/__init__.py
+++ telegram/handlers/messages/__init__.py
@@ -1,3 +1,4 @@
+from aiogram import Router
 from .text import router as text_router
 from .voice import router as voice_router
 from .photo import router as photo_router
@@ -5,6 +6,11 @@ from .video_note import router as video_note_router
 from .document import router as document_router
 from .sticker import router as sticker_router
 from .transcribe import router as transcribe_router
+from .chat import router as chat_router
+
+router = Router()
+router.include_router(transcribe_router)
+router.include_router(chat_router)
 
 message_routers = [
     text_router,
@@ -13,5 +19,4 @@ message_routers = [
     photo_router,
     document_router,
     sticker_router,
-    transcribe_router,
 ]
